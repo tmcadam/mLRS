561c6d2ebac70f9e676476be57967b1776041fa4
olliw42 <waldmanns@gmx.de>
2026-04-08 07:22:45 +0200

    common, sx-drivers, lr20xx driver, comment tidies (nfc)

--- mLRS/Common/sx-drivers/lr20xx_driver.h
+++ mLRS/Common/sx-drivers/lr20xx_driver.h
@@ -7,6 +7,7 @@
 //*******************************************************
 // Configuration defines:
 // #define POWER_USE_DEFAULT_RFPOWER_CALC
+// #define SX_TCXO_VOLTAGE
 //*******************************************************
 #ifndef LR20XX_DRIVER_H
 #define LR20XX_DRIVER_H
@@ -281,9 +282,9 @@ class Lr20xxDriverCommon : public Lr20xxDriverBase
 
 //??        SetRegMode(LR20XX_SIMO_USAGE_NORMAL); // Attention: requires DCDC workaround
 
-#ifdef LR_TCXO_VOLTAGE
-        SetTcxoMode(LR_TCXO_VOLTAGE, 64000); // 2 ms
-        ClearErrors(); // XOSC_START_ERR is raised after SetTcxoMode, clear it
+#ifdef SX_TCXO_VOLTAGE
+        SetTcxoMode(SX_TCXO_VOLTAGE, 64000); // 2 ms // functions correctly only when chip is in Standby RC mode
+        ClearErrors(); // we found: XOSC_START_ERR is (often) raised after SetTcxoMode, clear it
 #endif
 
         SetRxTxFallbackMode(LR20XX_RX_TX_FALLBACK_MODE_FS);
